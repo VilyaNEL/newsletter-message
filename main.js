5edcbca25cff3033b4ddfac42abb69e7543ffa22
VilyaNEL <vasyltsuni@gmail.com>
2023-07-28 20:12:24 +0300

    we created page

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+const button = document.querySelector(".button");
+const success = document.querySelector(".success");
+const subscribing = document.querySelector(".subscribing");
+const dismiss = document.querySelector(".dismiss");
+const email = document.querySelector(".email");
+const updatedEmail = document.querySelector(".email-tfs");
 
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+  let isValid = validateEmail(email.value);
+  console.log(isValid);
+  if (isValid) {
+    subscribing.setAttribute("aria-hidden", "true");
+    success.setAttribute("aria-hidden", "false");
+    updatedEmail.innerHTML = email.value;
+    email.value = "";
+  }
+});
+
+dismiss.addEventListener("click", function (event) {
+  subscribing.setAttribute("aria-hidden", "false");
+  success.setAttribute("aria-hidden", "true");
+});
+
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
